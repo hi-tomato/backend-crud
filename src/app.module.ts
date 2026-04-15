ef03c60d3cbf425aefe76e3d93887b5a3d75ac52
이승준 <tmdwns092727@nate.com>
2026-04-15 15:12:36 +0900

    👔 Update: AppModule에 Posts, Common 모듈 등록

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { PostsModule } from './posts/posts.module';
 import { UsersModule } from './users/users.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
   imports: [
@@ -23,6 +25,8 @@ import { UsersModule } from './users/users.module';
     }),
     UsersModule,
     AuthModule,
+    PostsModule,
+    CommonModule,
   ],
   controllers: [AppController],
   providers: [AppService],
